85b7ab2c474e49703af5374dc828b89879da1fda
JounQin <admin@1stg.me>
2024-01-18 16:15:47 +0800

    feat: add extractDataFromResponse util for better data from response

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,65 @@
-export type Nullable<T> = T | null | undefined
+export type Nil = null | undefined | void
+
+export type Nilable<T> = Nil | T
+
+export type Readonlyable<T> = Readonly<T> | T
+
+export type AnyArray<T = unknown> = Readonlyable<T[]>
+
+export type Arrayable<T, R extends boolean = false> = [R] extends [never]
+  ? T | T[]
+  : R extends true
+  ? Readonly<T> | readonly T[]
+  : R extends false
+  ? AnyArray<T> | Readonlyable<T>
+  : never
 
 export type ValueOf<T> = T[keyof T]
 
-export type URLSearchParametersInit = ConstructorParameters<
+export type URLSearchParamsInit = ConstructorParameters<
   typeof URLSearchParams
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 >[0]
 
-export type URLSearchParametersOptions =
-  | Record<string, Nullable<number | string>>
-  | URLSearchParametersInit
+export type URLSearchParamsOptions =
+  | Record<string, Nilable<Arrayable<number | string>>>
+  | URLSearchParamsInit
   | object
+
+export const ApiMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+} as const
+
+export type ApiMethod = ValueOf<typeof ApiMethod>
+
+export interface FetchApiBaseOptions
+  extends Omit<RequestInit, 'body' | 'method'> {
+  method?: ApiMethod
+  body?: BodyInit | object
+  query?: URLSearchParamsOptions
+  json?: boolean
+}
+
+export type ResponseType = 'arrayBuffer' | 'blob' | 'json' | 'text' | null
+
+export interface FetchApiOptions extends FetchApiBaseOptions {
+  type?: ResponseType
+}
+
+export interface InterceptorRequest extends FetchApiOptions {
+  url: string
+}
+
+export type ApiInterceptor = (
+  request: InterceptorRequest,
+  next: (request: InterceptorRequest) => PromiseLike<Response>,
+) => PromiseLike<Response> | Response
+
+export interface ResponseError<T = never> extends Error {
+  data?: T | null
+  response?: Response | null
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,14 @@
 import {
-  Nullable,
-  URLSearchParametersInit,
-  URLSearchParametersOptions,
+  Nilable,
+  ResponseType,
+  URLSearchParamsOptions,
   ValueOf,
 } from './types.js'
 
 export const CONTENT_TYPE = 'Content-Type'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
-const { toString } = Object.prototype // type-coverage:ignore-line - TODO: report bug
+const { toString } = Object.prototype // type-coverage:ignore-line -- https://github.com/plantain-00/type-coverage/issues/133
 
 const objectTag = '[object Object]'
 
@@ -22,7 +22,7 @@ export const cleanNilValues = <T = unknown>(input: T, empty?: boolean): T => {
 
   for (const _key of Object.keys(input)) {
     const key = _key as keyof T
-    const value = input[key] as Nullable<ValueOf<T>>
+    const value = input[key] as Nilable<ValueOf<T>>
     if (empty ? !value : value == null) {
       delete input[key]
     } else {
@@ -33,12 +33,78 @@ export const cleanNilValues = <T = unknown>(input: T, empty?: boolean): T => {
   return input
 }
 
-export const normalizeUrl = (
-  url: string,
-  query?: URLSearchParametersOptions,
-) => {
-  const search = new URLSearchParams(
-    cleanNilValues(query, true) as URLSearchParametersInit,
-  ).toString()
+export const normalizeUrl = (url: string, query?: URLSearchParamsOptions) => {
+  const cleanedQuery = cleanNilValues(query, true)
+  const searchParams = new URLSearchParams()
+  if (isPlainObject(cleanedQuery)) {
+    for (const [
+      key,
+      // type-coverage:ignore-next-line -- cannot control
+      _value,
+    ] of Object.entries(cleanedQuery)) {
+      const value = _value as unknown
+      if (Array.isArray(value)) {
+        const items = value as unknown[]
+        for (const item of items) {
+          searchParams.append(key, String(item))
+        }
+      } else {
+        searchParams.set(key, String(value))
+      }
+    }
+  }
+  const search = searchParams.toString()
   return search ? url + (url.includes('?') ? '&' : '?') + search : url
+}
+
+export async function extractDataFromResponse(
+  res: Response,
+  type: null,
+): Promise<Response>
+export async function extractDataFromResponse(
+  res: Response,
+  type: 'arrayBuffer',
+): Promise<ArrayBuffer>
+export async function extractDataFromResponse(
+  res: Response,
+  type: 'blob',
+): Promise<Blob>
+export async function extractDataFromResponse<T>(
+  res: Response,
+  type: 'json',
+): Promise<T>
+export async function extractDataFromResponse(
+  res: Response,
+  type: 'text',
+): Promise<string>
+export async function extractDataFromResponse(
+  res: Response,
+  type: ResponseType,
+): Promise<unknown>
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export async function extractDataFromResponse(
+  res: Response,
+  type: ResponseType,
+) {
+  let data: unknown
+  if (type != null) {
+    if (type === 'json' || type === 'text') {
+      try {
+        // data could be empty text
+        data = await res.clone().text()
+      } catch {}
+      if (type === 'json' && (data = (data as string).trim())) {
+        try {
+          data = JSON.parse(data as string)
+        } catch {}
+      }
+    } else {
+      try {
+        data = await res.clone()[type]()
+      } catch {
+        data = await res.clone().text()
+      }
+    }
+  }
+  return data
 }
