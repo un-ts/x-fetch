4c3976e448a21dc624a97d70aff22b9ce5bff96b
JounQin <admin@1stg.me>
2025-04-06 22:33:49 +0800

    docs: add shiki highlighting support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import mdx from '@mdx-js/rollup'
+import rehypeShiki from '@shikijs/rehype'
 import react from '@vitejs/plugin-react-swc'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
@@ -8,7 +9,13 @@ export default defineConfig({
   plugins: [
     mdx({
       remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypeSlug],
+      rehypePlugins: [
+        [
+          rehypeShiki,
+          { themes: { light: 'github-light', dark: 'github-dark' } },
+        ],
+        rehypeSlug,
+      ],
     }),
     react(),
   ],
